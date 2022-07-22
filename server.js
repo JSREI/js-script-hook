be6dd14cc5d6c845e5092cf1cc5b69d3323d93fc
CC11001100 <CC11001100@qq.com>
2022-07-23 02:26:40 +0800

    init

---
+++ server.js
@@ -0,0 +1,16 @@
+const http = require("http");
+const url = require("url");
+
+// 测试用的，忽略即可
+
+http.createServer((request, response) => {
+    response.writeHead(200, {"Content-Type": "application/javascript"});
+    const jsonpCallbackFunctionName = url.parse(request.url, true).query.jsonp_callback;
+    console.log(`jsonpCallbackFunctionName = ${jsonpCallbackFunctionName}`);
+    const result = {
+        "foo": "blablabla",
+        "bar": Math.random()
+    };
+    // 客户端会自动执行这个函数来处理结果
+    response.end(`${jsonpCallbackFunctionName}(${JSON.stringify(result)})`);
+}).listen(10010);
