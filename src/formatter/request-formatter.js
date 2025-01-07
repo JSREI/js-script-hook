6294b972778e703901f6070231518e5214ae000a
CC11001100 <CC11001100@qq.com>
2025-01-08 01:49:31 +0800

    fix: 移除request formatter的打印

--- src/formatter/request-formatter.js
+++ src/formatter/request-formatter.js
@@ -18,52 +18,6 @@ class RequestFormatter {
         const requestContext = scriptContext.requestContext;
         const language = getLanguage(getGlobalConfig().language);
 
-        // 提示词：
-        // 我需要写一些console.log的样式打印在控制台上，接下来会陆陆续续给你一些写样式的任务，球球你帮帮我
-
-        const valueStyle = `color: black; background: #E50000; font-size: 12px; font-weight: bold;`;
-        const normalStyle = `color: black; background: #FF6766; font-size: 12px;`;
-
-        const titleStyle = 'font-weight: bold; font-size: 16px; background: green; color: white; padding: 5px; border-radius: 3px;';
-
-        const length = 200;
-
-        const message = [
-            normalStyle, fillToLength(`-------------------------------------------------- Script Hook Captured Request --------------------------------------------------`, length) + "\n",
-            normalStyle, fillToLength(`${language.console.time}: ${new Date().toLocaleString()}`, length) + "\n",
-            normalStyle, fillToLength(`${language.console.requestId}: ${scriptContext.requestId}`, length) + "\n",
-            normalStyle, fillToLength(`${language.console.isJsonpRequest}: ${scriptContext.isJsonp()}`, length) + "\n",
-            normalStyle, fillToLength(`${language.console.hostname}: ${requestContext.hostname}`, length) + "\n",
-            normalStyle, fillToLength(`${language.console.path}: ${requestContext.path}`, length) + "\n",
-            normalStyle, fillToLength(`${language.console.hash}: ${requestContext.hash}`, length) + "\n",
-            normalStyle, fillToLength((() => {
-                const paramTitle = `${language.console.param}(${requestContext.params.length})`;
-                return paramTitle;
-            })(), length) + "\n",
-
-
-            // (() => {
-            //
-            //     let paramTitle = `${indentSpace}`;
-            //     if (!this.params.length) {
-            //         paramTitle += " do not have param.";
-            //     }
-            //     msgs.push(paramTitle);
-            //     for (let param of this.params) {
-            //         msgs.push(param.toHumanReadable(indent + 4));
-            //     }
-            //
-            //
-            //     if (this.hash) {
-            //         msgs.push()
-            //     }
-            //
-            //     return msgs.join("\n\n");
-            // })()
-
-        ];
-        console.log(genFormatArray(message), ...message);
-
         // 把参数以表格的形式打印
         // const data = this.convertParamsToTableData(language, requestContext.params);
         // console.table(data);
