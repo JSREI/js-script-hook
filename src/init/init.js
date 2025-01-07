1a1bd83d4b120b5ba3f86d99bb89d23503341ec9
CC11001100 <CC11001100@qq.com>
2025-01-08 01:17:47 +0800

    fix: 修正跳转打开时样式缺失的bug

--- src/init/init.js
+++ src/init/init.js
@@ -1,5 +1,4 @@
-const {registerMenu} = require("../config/ui/menu");
-const {Debugger} = require("../debugger/debugger");
+const {registerMenu, show} = require("../config/ui/menu");
 const {getUnsafeWindow} = require("../utils/scope-util");
 const {DocumentHook} = require("../hook/document-hook");
 const {initConfig, getGlobalConfig} = require("../config/config");
@@ -16,18 +15,12 @@ function init() {
     // 增加可视化的配置
     registerMenu();
 
-    // 增加一个测试断点
-    // const jsonpDebugger = new Debugger("test", true, "match-regexp", /xxxxx/);
-    // getGlobalConfig().addDebugger(jsonpDebugger);
-
     // 为document增加hook点
-    const unsafeWindow = getUnsafeWindow();
-    new DocumentHook(unsafeWindow.document).addHook();
+    new DocumentHook(getUnsafeWindow().document).addHook();
 
     if (GM_getValue("js-script-hook-open-configuration")) {
         GM_setValue("js-script-hook-open-configuration", false);
-        const configurationComponent = new ConfigurationComponent();
-        configurationComponent.show();
+        show();
     }
 
 }
