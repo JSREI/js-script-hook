400fec00e19176efa8bc39a18db967f0efddaa33
CC11001100 <CC11001100@qq.com>
2025-05-18 00:48:59 +0800

    修复切换语言后新增断点仍使用旧语言的问题

--- src/config/ui/component/debugger-manager/events.ts
+++ src/config/ui/component/debugger-manager/events.ts
@@ -3,6 +3,7 @@ import { DebuggerComponent, DebuggerConfig } from "../debugger";
 import { getGlobalConfig } from "../../../config";
 import { createNewDebuggerConfig } from "./utils";
 import { createLogger } from "../../../../logger";
+import { LanguageEventManager } from "../language-event";
 
 const debuggerManagerEventsLogger = createLogger('debugger-manager-events');
 
@@ -26,9 +27,12 @@ export function bindDebuggerManagerEvents(
                 const newDebuggerConfig = createNewDebuggerConfig();
                 const debuggerList = managerElement.querySelector("#js-script-hook-debugger-list");
                 
+                // 获取当前最新的语言配置
+                const currentLanguage = LanguageEventManager.getInstance().getCurrentLanguage() || language;
+                
                 if (debuggerList) {
                     debuggerComponents.push(debuggerComponent);
-                    debuggerList.appendChild(debuggerComponent.render(language, newDebuggerConfig as unknown as DebuggerConfig));
+                    debuggerList.appendChild(debuggerComponent.render(currentLanguage, newDebuggerConfig as unknown as DebuggerConfig));
                 }
 
                 getGlobalConfig().addDebugger(newDebuggerConfig);
