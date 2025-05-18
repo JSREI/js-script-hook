460de207d1112296961d1410ce8659775d010245
CC11001100 <CC11001100@qq.com>
2025-05-18 21:48:17 +0800

    refactor: 优化社区交流群组件，使用数据驱动的方式渲染二维码

--- script-hook-website/src/components/CommunityGroup.tsx
+++ script-hook-website/src/components/CommunityGroup.tsx
@@ -4,6 +4,25 @@ import Zoom from 'react-medium-image-zoom';
 import 'react-medium-image-zoom/dist/styles.css';
 
 const CommunityGroup: React.FC = () => {
+  const groups = [
+    {
+      title: '微信交流群',
+      image: 'https://cdn.jsdelivr.net/gh/JSREI/.github/profile/README.assets/image-20241016230653669.png',
+      description: '扫码加入逆向技术微信交流群'
+    },
+    {
+      title: '个人微信',
+      image: 'https://cdn.jsdelivr.net/gh/JSREI/.github/profile/README.assets/image-20231030132026541-7614065.png',
+      description: '群二维码过期，可加此微信发送【逆向群】'
+    },
+    {
+      title: 'Telegram交流群',
+      image: 'https://cdn.jsdelivr.net/gh/JSREI/.github/profile/README.assets/image-20241016231143315.png',
+      description: '点击或扫码加入TG交流群',
+      link: 'https://t.me/jsreijsrei'
+    }
+  ];
+
   return (
     <section id="community" className="community-section">
       <div className="container">
@@ -12,50 +31,31 @@ const CommunityGroup: React.FC = () => {
           <p>加入我们的交流群，获取最新信息与技术支持</p>
         </div>
         
-        <div className="community-content">
-          <div className="qr-group">
-            <div className="qr-item">
-              <h3>微信交流群</h3>
-              <div className="qr-image-container">
+        <div className="community-groups">
+          {groups.map((group, index) => (
+            <div key={index} className="community-group-item">
+              <h3>{group.title}</h3>
+              <div className="qr-code-wrapper">
                 <Zoom>
                   <img 
-                    src="https://cdn.jsdelivr.net/gh/JSREI/.github/profile/README.assets/image-20241016230653669.png" 
-                    alt="微信交流群二维码" 
-                    className="qr-image"
+                    src={group.image} 
+                    alt={`${group.title}二维码`} 
+                    className="qr-code-image"
                   />
                 </Zoom>
               </div>
-              <p>扫码加入逆向技术微信交流群</p>
+              <p className="group-description">
+                {group.link ? (
+                  <>
+                    <a href={group.link} target="_blank" rel="noopener noreferrer">点此</a>
+                    或扫码{group.description.split('点击或扫码')[1]}
+                  </>
+                ) : (
+                  group.description
+                )}
+              </p>
             </div>
-            
-            <div className="qr-item">
-              <h3>个人微信</h3>
-              <div className="qr-image-container">
-                <Zoom>
-                  <img 
-                    src="https://cdn.jsdelivr.net/gh/JSREI/.github/profile/README.assets/image-20231030132026541-7614065.png" 
-                    alt="个人微信二维码" 
-                    className="qr-image"
-                  />
-                </Zoom>
-              </div>
-              <p>群二维码过期，可加此微信发送【逆向群】</p>
-            </div>
-            
-            <div className="qr-item">
-              <h3>Telegram交流群</h3>
-              <div className="qr-image-container">
-                <Zoom>
-                  <img 
-                    src="https://cdn.jsdelivr.net/gh/JSREI/.github/profile/README.assets/image-20241016231143315.png" 
-                    alt="Telegram群二维码" 
-                    className="qr-image"
-                  />
-                </Zoom>
-              </div>
-              <p><a href="https://t.me/jsreijsrei" target="_blank" rel="noopener noreferrer">点此</a>或扫码加入TG交流群</p>
-            </div>
-          </div>
+          ))}
         </div>
       </div>
     </section>
